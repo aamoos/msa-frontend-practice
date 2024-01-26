941cae1e368ec1031cf22f650eee8a796c3611f1
aamoos <aamoos@naver.com>
2024-01-26 17:43:46 +0900

    axios 전역화 개발중

--- src/components/axios/InsertComponent.vue
+++ src/components/axios/InsertComponent.vue
@@ -1,9 +1,84 @@
 <template>
-    <div>
-        insert axios
-    </div>
+  <a-form
+    ref="formRef"
+    :model="formState"
+    :rules="rules"
+    :label-col="labelCol"
+    :wrapper-col="wrapperCol"
+  >
+    <a-form-item ref="name" label="title" name="name">
+      <a-input v-model:value="formState.name" />
+    </a-form-item>
+   
+    <a-form-item label="body" name="desc">
+      <a-textarea v-model:value="formState.desc" />
+    </a-form-item>
+    <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+      <a-button type="primary" @click="onSubmit">Create</a-button>
+      <a-button style="margin-left: 10px" @click="resetForm">Reset</a-button>
+    </a-form-item>
+  </a-form>
 </template>
-    
-<script>
+<script setup>
+import { reactive, ref, toRaw } from 'vue';
+import axios from 'axios'
+
+const formRef = ref();
+const labelCol = {
+  span: 5,
+};
+const wrapperCol = {
+  span: 13,
+};
+const formState = reactive({
+  name: '',
+  desc: '',
+});
+const rules = {
+  name: [
+    {
+      required: true,
+      message: '제목을 입력해주세요.',
+      trigger: 'change',
+    },
+    {
+      min: 1,
+      max: 20,
+      message: '제목은 20자 이내로 사용할수 있습니다.',
+      trigger: 'blur',
+    },
+  ],
+  desc: [
+    {
+      required: true,
+      message: '내용을 입력해주세요.',
+      trigger: 'blur',
+    },
+  ],
+};
+const onSubmit = () => {
+  formRef.value
+    .validate()
+    .then(() => {
+        console.log('values', formState, toRaw(formState));
+        save();
+    })
+    .catch(error => {
+      console.log('error', error);
+    });
+};
+const resetForm = () => {
+  formRef.value.resetFields();
+};
+
+//저장
+const save = async () => {
+    const response = await axios.post('/api/axios/save', {
+        title: formState.name,
+        body: formState.desc
+    });
+    console.log(response);
+    // Handle the response here
+};
 
 </script>
--- src/components/axios/ListComponent.vue
+++ src/components/axios/ListComponent.vue
@@ -1,9 +1,36 @@
 <template>
-    <div>
-        list axios
-    </div>
+  <a-list item-layout="horizontal" :data-source="data">
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <a-list-item-meta :description="item.body">
+          <template #title>
+            <a href="https://www.antdv.com/">{{ item.title }}</a>
+          </template>
+          <template #avatar>
+            <a-avatar src="https://joeschmoe.io/api/v1/random" />
+          </template>
+        </a-list-item-meta>
+      </a-list-item>
+    </template>
+  </a-list>
 </template>
-    
-<script>
-
+<script setup>
+const data = [
+  {
+    title: 'Ant Design Title 1',
+    body: 'Ant Design Body 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+    body: 'Ant Design Body 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+    body: 'Ant Design Body 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+    body: 'Ant Design Body 4',
+  },
+];
 </script>
