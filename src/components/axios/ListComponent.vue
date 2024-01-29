959a59a62679660ae4feada92c99f4d68962802f
aamoos <aamoos@naver.com>
2024-01-29 18:08:57 +0900

    list axios 추가

--- src/components/axios/ListComponent.vue
+++ src/components/axios/ListComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-list item-layout="horizontal" :data-source="data">
+  <a-list item-layout="horizontal" :data-source="dataSource">
     <template #renderItem="{ item }">
       <a-list-item>
         <a-list-item-meta :description="item.body">
@@ -15,22 +15,22 @@
   </a-list>
 </template>
 <script setup>
-const data = [
-  {
-    title: 'Ant Design Title 1',
-    body: 'Ant Design Body 1',
-  },
-  {
-    title: 'Ant Design Title 2',
-    body: 'Ant Design Body 2',
-  },
-  {
-    title: 'Ant Design Title 3',
-    body: 'Ant Design Body 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-    body: 'Ant Design Body 4',
-  },
-];
+import { inject, ref, onMounted } from 'vue';
+const axios = inject('axios');
+const dataSource = ref([]);
+
+const fetchData = async () => {
+    console.log(axios);
+    try {
+        const response = await axios.get('/api/v1/axios/list');
+        dataSource.value = response.data.data;
+        // Handle the response here
+    } catch (error) {
+        console.error('Error occurred while select:', error);
+    }
+};
+
+// Call fetchData when the component is mounted
+onMounted(fetchData);
+
 </script>
