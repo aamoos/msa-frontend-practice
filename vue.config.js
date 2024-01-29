7d90202f25230709a4e9e1062785f379bfc860d9
aamoos <aamoos@naver.com>
2024-01-29 17:41:13 +0900

    devserver target 제거

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 
+// module.exports = defineConfig({
+//   lintOnSave: false,
+//   devServer: {
+//     proxy: {
+//       '/api/v1': {
+//         target: 'http://localhost:3000',  // 프록시 대상
+//         changeOrigin: true,               // 필요한 경우, 호스트 헤더 변경
+//         pathRewrite: {                    // 필요한 경우, 경로 재작성
+//           '^/api/v1': ''                  // 실제 요청에서 '/api/v1'를 삭제
+//         }
+//       }
+//     }
+//   }
+// })
+
 module.exports = defineConfig({
-  lintOnSave: false,
   devServer: {
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost:3000',  // 프록시 대상
-        changeOrigin: true,               // 필요한 경우, 호스트 헤더 변경
-        pathRewrite: {                    // 필요한 경우, 경로 재작성
-          '^/api/v1': ''                  // 실제 요청에서 '/api/v1'를 삭제
-        }
-      }
-    }
+    proxy :'http://localhost:3000'
   }
 })
